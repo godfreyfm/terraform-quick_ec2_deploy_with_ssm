02c3dd7e6d45445844df4edebc2ce9b0c8af84bb
gmaiwun <gmaiwun@gmail.com>
2024-07-18 17:59:25 -0500

    Created the ebs volume module

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "session_manager" {
-  source  = "./module/session_manager"
+  source  = "./modules/session_manager"
   project = var.project
 }
 
 
 module "ec2_instance" {
-  source                    = "./module/ec2_instance"
+  source                    = "./modules/ec2_instance"
   region                    = var.region
   availability_zone         = var.availability_zone
   instance_type             = var.instance_type
@@ -15,4 +15,16 @@ module "ec2_instance" {
   public_cidr               = var.public_cidr
   vpc_cidr                  = var.vpc_cidr
   iam_instance_profile_name = module.session_manager.iam_instance_profile_name
+}
+
+module "ebs_volume" {
+  depends_on        = [module.ec2_instance]
+  source            = "./modules/ebs_volume"
+  volume_size       = var.volume_size
+  volume_type       = var.volume_type
+  availability_zone = var.ebs_availability_zone
+  instance_id       = var.instance_id
+  device_name       = var.device_name
+  count = var.create_ebs_volume ? 1 : 0
+
 }
---
+++ modules/ebs_volume_attachment/variables.tf
@@ -0,0 +1,36 @@
+variable "volume_size" {
+  description = "The size of the EBS volume in GB."
+  type        = number
+}
+
+variable "volume_type" {
+  description = "The type of the EBS volume."
+  type        = string
+  default     = "gp2"
+}
+
+variable "ebs_availability_zone" {
+  description = "The availability zone where the EBS volume will be created."
+  type        = string
+}
+
+variable "instance_id" {
+  description = "The ID of the EC2 instance to attach the EBS volume to."
+  type        = string
+}
+
+variable "device_name" {
+  description = "The device name to expose to the EC2 instance."
+  type        = string
+}
+
+variable "project" {
+  description = "The project name."
+  type        = string
+}
+
+variable "create_ebs_volume" {
+  description = "Flag to determine whether to create the EBS volume."
+  type        = bool
+  default     = true
+}
